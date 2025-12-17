a101ae83db0aba02ccdcd29d3b7c696e2e6c9589
Tejprakash01 <tejprakashtak@gmail.com>
2025-12-17 13:02:42 +0530

    Fix Django settings initialization for Channels on Render

--- blog_project/asgi.py
+++ blog_project/asgi.py
@@ -1,14 +1,21 @@
 import os
-from django.core.asgi import get_asgi_application
+import django
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import chat.routing
+from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blog_project.settings")
+
+django.setup()
+
+import chat.routing 
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
-        URLRouter(chat.routing.websocket_urlpatterns)
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
     ),
 })
