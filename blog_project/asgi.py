320d2cd66fbe0e36d62e0edec686d501474609a4
Tejprakash01 <tejprakashtak@gmail.com>
2025-12-17 12:23:31 +0530

    Fix ALLOWED_HOSTS for Render

--- blog_project/asgi.py
+++ blog_project/asgi.py
@@ -1,13 +1,10 @@
 import os
-import django
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog_project.settings')
-django.setup()
-
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import chat.routing
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blog_project.settings")
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
